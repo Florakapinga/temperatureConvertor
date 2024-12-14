4e9b8d9bf33cff6464eec286958f5ef49c2a48ce
kapinga <erickfloriana@gmail.com>
2024-12-14 11:38:09 -0800

    third commit

--- temperature.cpp
+++ temperature.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
 using namespace std;
-void convertTemperature(double temperature, char scale) {
-    if (scale == 'C' || scale == 'c') {
+void convertTemperature(double temperature, char scale)
+{
+    if (scale == 'C' || scale == 'c')
+    {
+        double fahrenheit = (temperature * 9.0 / 5.0) + 32.0;
+        cout << temperature << "°C is " << fahrenheit << "°F" << endl;
